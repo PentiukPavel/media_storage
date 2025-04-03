4f91872f242396abc29e11791f205fe6f342c49f
PentiukPavel <pentukpavel@yandex.ru>
2025-04-03 15:23:43 +0300

    ref: correct uploading files

--- src/services/files.py
+++ src/services/files.py
@@ -1,6 +1,6 @@
-from pathlib import Path
+import os
 
-import shutil
+import aiofiles
 
 from fastapi import UploadFile
 
@@ -24,20 +24,19 @@ class MediaFilesService:
         current_user: User,
         file: UploadFile,
     ):
+        ext = file.filename.split(".")[-1]
+        path = os.path.join(
+            settings.STORAGE_LOCATION,
+            f"{filename}.{ext}",
+        )
         data = {
             "filename": filename,
             "owner_id": current_user.id,
+            "file_url": str(path),
         }
-        filename = file.filename
-        ext = filename.split(".")[-1]
-        path = Path(
-            settings.STORAGE_LOCATION,
-            f"{data["owner_id"]}",
-            f"{data["filename"]}.{ext}\\",
-        )
-        data["file_url"] = str(path)
-        async with open(path, "wb+") as media_file:
-            shutil.copyfileobj(file.file, media_file)
+        async with aiofiles.open(path, "wb") as media_file:
+            content = await file.read()
+            await media_file.write(content)
         async with self.uow:
             media_file = await self.uow.media.create_media_file(data)
             await self.uow.commit()
